e0daf76761e599e14463765e50814c474acf3ec3
tubi55 <tubi55@nate.com>
2024-09-24 11:51:55 +0900

    유튜브 데이터 호출

--- youtube.js
+++ youtube.js
@@ -1,16 +1,17 @@
 /*
-  서버에서 "실시간으로" 외부 데이터를 내 웹 페이지 문서로 가져오는 방법
-
-  fetch('요청URL').then(data=> data.json()).then(json=>{
-    //요청한 서버쪽으로부터 성공적으로 데이터를 전달받으면 json이라는 매개변수 데이터 확인 가능
-    console.log(json)  
-  })
+  QueryString : 기본 요청 URL뒤에 문자열형태로 옵션값을 달아서 서버에 요청하는 형태
+  https://www.abc.com?pwd=1234&name=abc;
+  www.abc.com //기본 요청 URL
+  ?뒤에 있는 key=value값 문자열 형태로 지정한 추가 요청사항 
+  요청사항이 여러개일때에는 &로 구분
 */
 
-console.log("youtube");
 const api_key = "AIzaSyDfF904vE_uzyNlnhKgAyUmNWV9U5vTxZ0";
+const pid = "PLHtvRFLN5v-WSMMckC0kyUPBzYhXVREHD";
 
-fetch("/data.json")
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${pid}&key=${api_key}`;
+
+fetch(url)
   .then((data) => data.json())
   .then((json) => {
     console.log(json);
